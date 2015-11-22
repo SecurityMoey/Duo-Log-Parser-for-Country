78960ace67f5c88d8c32ceb1b40d5d3ec739d070
Moey (Michael) <SecurityMoey@users.noreply.github.com>
2015-11-22 04:11:45 -0600

    code clean up...
    still getting the following error

--- duologparserforcountry.py
+++ duologparserforcountry.py
@@ -1,52 +1,54 @@
 #!/usr/bin/env python
 
-import sys, os
+import csv
+import os
+import sys
+
 
 try:
     import pygeoip
 except ImportError:
-    print "Error: GeoIP Python Module is required and not installed. Please run 'pip install pygeoip to install"
-    sys.exit()
-
-try:
-    import csv
-except ImportError:
-    print "Error: csv Module is required and not installed. Please run 'pip install csv' to install"
+    print "Error: GeoIP Python Module is not installed."
+    print "Please run 'pip install pygeoip' in your virtualenv."
     sys.exit()
 
 try:
     with open('/usr/local/GeoIP.dat') as file:
         pass
 except IOError as e:
-    print "GeoIP.dat file is not located in /usr/local.  Please obtain the file and ensure it's in the directory location. It can be found at http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz"
+    print "'GeoIP.dat' is not located in /usr/local"
+    print "Please obtain the file and ensure it's in the directory location."
+    print "It can be found at the following URL:"
+    print "https://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz"
+    sys.exit()
 
-print "First we need to know the name of the Authentication log \n Please make sure the file is in the directory you are running this script."
+print "First we need to know the name of the Authentication log"
+print "Please make sure the file is in the directory you are running this script."
 
 sfile = raw_input("Duo Authentication Log File name: ")
-ofile = r'duologswcountry'
+ofile = 'duologswcountry'
 fields = ["Timestamp", "User", "IP Address"]
 
-with open(sfile) as infile, open(ofile, "wb") as outfile:
+with open(sfile, "rb") as infile, open(ofile, "wb") as outfile:
     r = csv.DictReader(infile)
     w = csv.DictWriter(outfile, fields, extrasaction="ignore")
     w.writeheader()
     for row in r:
         w.writerow(row)
 
-filename = ofile
 input_file = csv.DictReader(open(ofile))
-ip_add = (row["IP Address"])
 
-def country(filename):        
-    gi = pygeoip.GeoIP('/usr/local/GeoIP.dat')
-    return gi.country_name_by_addr(ip_add)
+gi = pygeoip.GeoIP('/usr/local/GeoIP.dat')
 
-with open(filename, 'rb') as handle:
+def country(addr):
+    return gi.country_name_by_addr(addr)
+
+with open(ofile, 'rb') as handle:
     reader = csv.reader(handle)
-    writer = csv.writer(open('%s.csv' % filename, 'w'))
+    writer = csv.writer(open('%s.csv' % ofile, 'w'))
     for row in reader:
-        writer.writerow(row + [ country(ip_add) ])
+        addr = int(row['IP Address'])
+        writer.writerow(row + [ country(addr) ])
 
-os.remove(ofile)
-
-print r"Script Complete. Please review duologswcountry.csv to see the results"
+print "Script Complete."
+print "Please review duologswcountry.csv to see the results"
